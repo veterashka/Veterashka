33760f210371c7e0f92d7ad39f307e17ae2aeb04
name <name@gmail.com>
2024-01-29 15:05:31 +0300

    Zadanie_wan: A page that, when opened, will change to a random background every 300 ms. Zadanie_two: A three-column SQL table filled with three records.

--- Number_two.py
+++ Number_two.py
@@ -2,5 +2,5 @@ import sqlite3
 conn = sqlite3.connect('Zad_two.db')
 cursor = conn.cursor()
 cursor.execute('''CREATE TABLE IF NOT EXISTS Tab_1(col_1 TEXT, col_2 TEXT, col_3 TEXT)''')
-cursor.execute('''INSERT INTO Tab_1(col_1, col_2, col_3) VALUES ('wan', 'two:', '"three"')''')
+cursor.execute('''INSERT INTO Tab_1(col_1, col_2, col_3) VALUES ('Hello', 'teacher:', '"Egor"')''')
 conn.commit()
